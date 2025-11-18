0f482f4c5a88dab210854dc2a27881b6c6814197
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-18 23:23:43 +0545

    feat: add class for user entity

---
+++ src/Amnil.TaskManagement.Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scriban.Functions;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Amnil.TaskManagement.Entities
+{
+    public class User : AuditedAggregateRoot<Guid>
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        protected User() { }
+
+        public User(string userName, string email)
+        {
+            UserName = userName;
+            Email = email;
+        }
+
+        public void Update(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
