81a3d790ffc1c1e724f783c86d23f13655d81d35
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-18 23:24:34 +0545

    feat: add interface for user service

---
+++ src/Amnil.TaskManagement.Application.Contracts/Interfaces/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amnil.TaskManagement.DTOs;
+using Amnil.TaskManagement.DTOs.Users;
+using Volo.Abp.Application.Services;
+
+namespace Amnil.TaskManagement.Interfaces
+{
+    public interface IUserService : IApplicationService
+    {
+        Task RegisterUserAsync(RegisterUpdateUserDto registerDto);
+        Task UpdateUserAsync(Guid userId, RegisterUpdateUserDto updateDto);
+        Task DeleteUserAsync(Guid userId);
+        Task<string> LoginAsync(LoginDto loginDto);
+        Task<List<TaskDto>> GetMyTasksAsync();
+
+    }
+}
