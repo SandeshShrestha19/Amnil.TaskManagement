86966734f615dfd9584898656c4c1ef3608e7686
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-18 15:07:01 +0545

    feat: create dto for project related operation

---
+++ src/Amnil.TaskManagement.Domain/Entities/Project.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Amnil.TaskManagement.Entities
+{
+    public class Project : AuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        protected Project() { }
+
+        public Project(string name, string description, DateTime? startDate, DateTime? endDate)
+        {
+            Name = name;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+        public void Update(string name, string description, DateTime? startDate, DateTime? endDate)
+        {
+            Name = name;
+            Description = description;
+            StartDate = start;
+            EndDate = end;
+        }
+    }
+}
